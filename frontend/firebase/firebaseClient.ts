bdad1ba40e0550105b0e95f7090f322f16fd8344
keith <68919845+keithfykai@users.noreply.github.com>
2026-01-29 10:36:04 +0800

    Updated Frontend Test

--- frontend/firebase/firebaseClient.ts
+++ frontend/firebase/firebaseClient.ts
@@ -1,18 +1,20 @@
-// "use client";
 
-// import { getAuth } from "firebase/auth";
-// import { getAnalytics } from "firebase/analytics";
-// import { app } from "./firebaseConfig";
 
-// let authInstance: ReturnType<typeof getAuth>;
-// let analyticsInstance: ReturnType<typeof getAnalytics>;
+import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+import { app } from "./firebaseConfig";
 
-// export const getAuthClient = () => {
-//   if (!authInstance) authInstance = getAuth(app);
-//   return authInstance;
-// };
+let authInstance: ReturnType<typeof getAuth>;
+let analyticsInstance: ReturnType<typeof getAnalytics>;
 
-// export const getAnalyticsClient = () => {
-//   if (!analyticsInstance) analyticsInstance = getAnalytics(app);
-//   return analyticsInstance;
-// };
+export const getAuthClient = () => {
+  if (!authInstance) authInstance = getAuth(app);
+  return authInstance;
+};
+
+export const getAnalyticsClient = () => {
+  if (!analyticsInstance) analyticsInstance = getAnalytics(app);
+  return analyticsInstance;
+};
+
+export const auth = getAuth(app);
--- frontend/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="p-6 text-center bg-white dark:bg-black">
-      <p className="text-sm text-slate-600 dark:text-slate-400">
-        &copy; {new Date().getFullYear()} Eduble. Created by Keith Lim.
-      </p>
-    </footer>
-  );
-}
